ab1b2417c9c718c3fa882b40ba1e53403354bb2d
Lee YoungMin <miniyoung37@gmail.com>
2023-04-08 14:35:22 +0900

    flutter button style ui 추가

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -18,6 +18,7 @@ class App extends StatelessWidget {
               horizontal: 40,
             ),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(
                   height: 80,
@@ -47,6 +48,51 @@ class App extends StatelessWidget {
                     )
                   ],
                 ),
+                SizedBox(
+                  height: 120,
+                ),
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.8),
+                    fontSize: 22,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  '\$5 194 234',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 48,
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: -2,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(45),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 20,
+                          horizontal: 50,
+                        ),
+                        child: Text('Transfer',
+                            style: TextStyle(
+                              fontSize: 18,
+                            )),
+                      ),
+                    ),
+                  ],
+                )
               ],
             )),
       ),
