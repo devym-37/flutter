a5ef05d43e0d27cdc0bdee3a9fa641eeb1d7bd06
Lee YoungMin <miniyoung37@gmail.com>
2023-04-13 00:20:05 +0900

    pomodoros 앱 ui 기본 구성

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,103 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
-  runApp(const App());
+  runApp(App());
 }
 
 // flutter SDK 3개의 core Widget중 하나를 상속 받아야한다
-class App extends StatefulWidget {
-  const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  int counter = 0;
-
-  List<int> numbers = [];
-
-  void onIncreaseButton() {
-    setState(() {
-      counter++;
-    });
-  }
-
-  void onDecreaseButton() {
-    setState(() {
-      counter--;
-    });
-  }
-
-  void onClickAddNumber() {
-    setState(() {
-      numbers.add(numbers.length);
-    });
-  }
-
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSwatch(backgroundColor: const Color(0xFFE7626C)),
         textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red,
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
           ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const LargeTitle(),
-              Text(
-                '$counter',
-                style: const TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              for (var number in numbers) Text('$number'),
-              IconButton(
-                onPressed: onClickAddNumber,
-                icon: const Icon(Icons.add_alarm_sharp),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: onIncreaseButton,
-                    icon: const Icon(Icons.plus_one),
-                  ),
-                  IconButton(
-                    onPressed: onDecreaseButton,
-                    icon: const Icon(Icons.exposure_minus_1_rounded),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class LargeTitle extends StatelessWidget {
-  const LargeTitle({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'Click Count',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of((context)).textTheme.titleLarge!.color,
-      ),
+      home: const HomeScreen(),
     );
   }
 }
