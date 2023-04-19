378c1fa67c5435762066f2ac3fc3ff386ba1954e
Lee YoungMin <miniyoung37@gmail.com>
2023-04-19 22:56:58 +0900

    webtoon api 연결 및 async await json 사용

---
+++ toonflix/lib/services/api_service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
+
+class ApiService {
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
+
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      List<WebtoonModel> webtoonInstances = [];
+      final List<dynamic> webtoons = jsonDecode(response.body);
+
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+
+      return webtoonInstances;
+    }
+    throw Error();
+  }
+}
