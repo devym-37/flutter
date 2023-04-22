bb695dc77e71595c1d50b89d4fc73e8d4d85f7bf
Lee YoungMin <miniyoung37@gmail.com>
2023-04-22 22:46:05 +0900

    detail / episode api 연결 model 추가

--- toonflix/lib/services/api_service.dart
+++ toonflix/lib/services/api_service.dart
@@ -3,6 +3,9 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon_model.dart';
 
+import '../models/webtoon_detail_modal.dart';
+import '../models/webtoon_episode_model.dart';
+
 class ApiService {
   static const String baseUrl =
       "https://webtoon-crawler.nomadcoders.workers.dev";
@@ -21,6 +24,34 @@ class ApiService {
       }
 
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      List<WebtoonEpisodeModel> webtoonEpisodes = [];
+      final episodes = jsonDecode(response.body);
+
+      for (var episode in episodes) {
+        webtoonEpisodes.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+
+      return webtoonEpisodes;
     }
     throw Error();
   }
